cd504c630f6bc7e6b23311f12fd906aa06396690
Monty _L <114133472+FengEternity@users.noreply.github.com>
2022-10-04 20:29:45 +0800

    Update fifth.py

--- fifth.py
+++ fifth.py
@@ -4,8 +4,18 @@
 表示字符'A'或'a'共出现了2次，字符'D'或'd'共出现了1次，依此类推。
 '''
 '''
+# 实现对列表的切片
+def dict_slice(adict, start, end):
+    keys = adict.keys()
+    dict_slice = {}
+    for k in list(keys)[start:end]:
+        dict_slice[k] = adict[k]
+    return dict_slice
+
+
 test = input("输入一个字符串：")
 test = test.lower()
+
 
 dict = {}.fromkeys((chr(i) for i in range(97,123)),0) #生成26个字母的字典
 
@@ -14,10 +24,10 @@ for j in test:
         dict[j] = 1
     else:
         dict[j] += 1
+        
+res = dict_slice(dict,0,26)
 
-print(dict.values())
-    
-# 保留问题：取出字典的前 n 个值
+print(res.values())
 '''
 
 
@@ -42,4 +52,40 @@ for i in range(len(temp)):
         print("%d * " % temp[i],end='')
     else:
         print("%d " % temp[i],end='')
+'''
+
+# 第 8 题
+'''
+验证命题：如果三个整数是 37 的倍数，则这个整数循环左移后得到的两个三位数也是 37 的倍数。\
+命题为真则输出“这是一个真命题”；命题为假则输出“这是一个假命题”。
+'''
+'''
+test = []
+
+# 生成所有 37 倍数的三位整数
+for i in range(0,1000):
+    if i*37 > 99 and i*37 < 1000:
+        test.append(i*37)
+    else:
+        continue
+res = []
+for j in test:
+    a = j//100 # 个位
+    b = j%100//10 # 十位 
+    c = j%100%10 # 百位
+
+    res_1 = b*100 + c*10 + a
+    res_2 = c*100 + a*10 + b
+    res.append(res_1)
+    res.append(res_2)
+    
+for k in res:
+    if k < 100 or k > 999: # 判断左移后存在 0 的数字
+        continue
+    if k%37 != 0 and k%37 != 0:
+        print("这是一个假命题")
+    else:
+        print("这是一个真命题")
+
+# 保留问题：如何只输出一个 “这是一个真命题”
 '''
