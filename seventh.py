45b0e463d93e236cbb7eb0397d662806e72d4321
Monty _L <feng_life@foxmail.com>
2022-11-15 22:03:24 +0800

    11/15 commit

--- seventh.py
+++ seventh.py
@@ -24,14 +24,14 @@ if __name__ == "__main__":
 '''
 
 # 2
-'''
-with open('date.txt','r+') as f:
-    lines = f.read().splitlines()  # 按行读取文件且忽略末尾换行
-    f.write("\n")
-    for line in lines:
-        reline = line[::-1]
-        f.write(f"{line}-{reline}\n")
-'''
+# '''
+# with open('date.txt','r+') as f:
+#     lines = f.read().splitlines()  # 按行读取文件且忽略末尾换行
+#     f.write("\n")
+#     for line in lines:
+#         reline = line[::-1]
+#         f.write(f"{line}-{reline}\n")
+# '''
 
 # 3
 '''
@@ -77,82 +77,82 @@ strip():
     用于移除字符串首尾指定的字符串，默认为空格和换行符
 '''
 '''
-with open('subtitles.srt','r+',encoding='utf-8') as f_read:
-    with open('7_4_result.txt','a+',encoding='utf-8') as f_write:
-        new = []
-        for line in f_read.readlines():
-            if line[0] not in list('\n0123456789') and line[-2] not in list('0123456789'):
-                new.append(line)
-        new = [ele.strip() for ele in new]
-
-        for line in new:
-            f_write.write(line+'\n')
-'''
-
-# 6
-'''
-with open('article.txt', 'r') as f:
-    article_str = f.read()
-    article_str = article_str.lower()
-    word_list = article_str.split()
-    pop_list = []
-    # print(len(word_list))
-    count = 0
-
-    for word in word_list:
-
-        # 將含有非26字母字符的元素輸出
-        # word_list:儲存全是字母的元素
-        # pop_list:儲存含非字母元素
-        # 輸出正確,則len(word_list)=len(word_list)+pop_list    (後一個word_list為pop后的)
-
-        for i in word:
-            if ord('a') <= ord(i) <= ord('z'):
-                continue
-            else:
-                word_list.pop(count)
-                pop_word = word.replace(i, '')
-                pop_list.append(pop_word)
-        count += 1
-    # print(len(word_list))
-    # print(len(pop_list))
-
-    res_list = word_list + pop_list
-    res = max(res_list,key=len)
-    print(res)
+# with open('subtitles.srt','r+',encoding='utf-8') as f_read:
+#     with open('7_4_result.txt','a+',encoding='utf-8') as f_write:
+#         new = []
+#         for line in f_read.readlines():
+#             if line[0] not in list('\n0123456789') and line[-2] not in list('0123456789'):
+#                 new.append(line)
+#         new = [ele.strip() for ele in new]
+# 
+#         for line in new:
+#             f_write.write(line+'\n')
+# '''
+#
+# # 6
+# '''
+# with open('article.txt', 'r') as f:
+#     article_str = f.read()
+#     article_str = article_str.lower()
+#     word_list = article_str.split()
+#     pop_list = []
+#     # print(len(word_list))
+#     count = 0
+#
+#     for word in word_list:
+#
+#         # 將含有非26字母字符的元素輸出
+#         # word_list:儲存全是字母的元素
+#         # pop_list:儲存含非字母元素
+#         # 輸出正確,則len(word_list)=len(word_list)+pop_list    (後一個word_list為pop后的)
+#
+#         for i in word:
+#             if ord('a') <= ord(i) <= ord('z'):
+#                 continue
+#             else:
+#                 word_list.pop(count)
+#                 pop_word = word.replace(i, '')
+#                 pop_list.append(pop_word)
+#         count += 1
+#     # print(len(word_list))
+#     # print(len(pop_list))
+#
+#     res_list = word_list + pop_list
+#     res = max(res_list,key=len)
+#     print(res)
 
 '''
 
 # 7
 
 '''
-def find(digit):
-    with open('date2.txt', 'r') as f:
-        sum_list = []
-        line_list  = f.read().splitlines()
-
-        for line in line_list:
-            digit_list = []
-
-            for i in line:
-                if ord(i) >= ord('0') and ord(i) <= ord('9'):
-                    digit_list.append(i)
-
-            sum = 0
-            for j in digit_list:
-                sum += int(j)
-            sum_list.append(sum)
-
-            if digit in sum_list:
-                print(f"行数：{int(sum_list.index(digit))+1}\n对应数据为:{line_list[int(sum_list.index(digit))]}")
-                return True
-
-
-if __name__ == "__main__":
-    digit = int(input("輸入待查詢整數："))
-    if find(digit) == None:
-        print("輸入數字不滿足題意")
-'''
+# def find(digit):
+#     with open('date2.txt', 'r') as f:
+#         sum_list = []
+#         line_list  = f.read().splitlines()
+#
+#         for line in line_list:
+#             digit_list = []
+#
+#             for i in line:
+#                 if ord(i) >= ord('0') and ord(i) <= ord('9'):
+#                     digit_list.append(i)
+#
+#             sum = 0
+#             for j in digit_list:
+#                 sum += int(j)
+#             sum_list.append(sum)
+#
+#             if digit in sum_list:
+#                 print(f"行数：{int(sum_list.index(digit))+1}\n对应数据为:{line_list[int(sum_list.index(digit))]}")
+#                 return True
+#
+#
+# if __name__ == "__main__":
+#     digit = int(input("輸入待查詢整數："))
+#     if find(digit) == None:
+#         print("輸入數字不滿足題意")
+# '''
 
 # 8
 with open('Blowing in the wind.txt', 'r+') as f:
--- tenth.py
+++ tenth.py
@@ -1,2 +1,48 @@
 # 1
+# import numpy as np
+#
+# test_np = np.array([(1, 2, 3, 4),
+#                     (5, 6, 7, 8),
+#                     (9, 10, 11, 12),
+#                     (13, 14, 15, 16)])
+#
+# all_sum = np.array(sum(sum(test_np)))
+# col_mean = np.mean(test_np,axis=0)
+# row_mean = np.mean(test_np,axis=1)
+#
+# res = np.hstack((all_sum, col_mean, row_mean))
+#     # vstack()是垂直方向上的堆叠，hstack()是水平方向上的堆叠
+# print(res)
+
+# 2
+# import numpy as np
+#
+#
+# def mul(f, g):
+#     print(np.poly1d(f)*np.poly1d(f))
+#     return np.poly1d(f)*np.poly1d(g)
+#
+#
+# if __name__ == "__main__":
+#     f = np.array([2., 0, -1, 1.])
+#     g = np.array([1., 2., 1.])
+#     mul(f, g)
+
+import matplotlib.pyplot as plt
 import numpy as np
+
+x1 = np.arange(0., 2., 0.1)
+y1 = np.sin(x1)
+
+x2 = np.arange(0., 2., 0.1)
+y2 = np.cos(x2)
+
+flg, ax = plt.subplots(2, 1, figsize=(15, 10))
+line, = ax[0].plot(x1, y1)
+line.set_dashes([5, 2])
+ax[0].set_title('y=sin(x)')
+
+line, = ax[1].plot(x2, y2)
+line.set_dashes([5, 2])
+ax[1].set_title('y=cos(x)')
+plt.show()
